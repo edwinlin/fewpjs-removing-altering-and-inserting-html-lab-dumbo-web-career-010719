3566fc25040b11866cbef355f578fc031f09e61c
Edwin Lin <edwinlin@gmail.com>
2019-02-19 05:39:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code here!
 let main = document.getElementById("main");
 main.remove();
-let newHeader = document.createElement('h1');
+let newH = document.createElement('h1');
 newHeader.id = 'victory';
 let newNode = document.createTextNode('YOUR-NAME is the champion');
-newHeader.appendChild(newNode);
+newH.appendChild(newNode);
 
+let newHeader = getElementById('victory');
