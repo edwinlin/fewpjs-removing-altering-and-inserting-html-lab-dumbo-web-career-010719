8ec2a417d83dbe67480a7ccda5e534d4e738e04f
Edwin Lin <edwinlin@gmail.com>
2019-02-19 05:25:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 let main = document.getElementById("main");
 main.remove();
-let newHeader = document.createElement('h1#victory');
+let newHeader = document.createElement('h1point#victory');
