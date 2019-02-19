0ff11bd6b9049ce4d9bf3c720df504cde7e3040f
Edwin Lin <edwinlin@gmail.com>
2019-02-19 05:36:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let main = document.getElementById("main");
 main.remove();
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-const YOUR-NAME = "Edwin";
-let newNode = document.createTextNode(`${YOUR-NAME} is the champion`);
+let newNode = document.createTextNode('YOUR-NAME is the champion');
 newHeader.appendChild(newNode);
 
