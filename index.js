59a055e573162f68d32da4e28f8fc28f2ea1194c
Edwin Lin <edwinlin@gmail.com>
2019-02-19 05:42:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let main = document.getElementById("main");
 main.remove();
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML
+newHeader.innerText = "Edwin is the champion";
